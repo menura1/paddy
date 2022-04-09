b968691223452d9db1fdb3cade4cd221afd0f387
DELL <ruchi.jayasena@gmail.com>
2022-04-09 18:04:26 +0530

    created contact us page

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -10,6 +10,61 @@ class ContactScreen extends StatefulWidget {
 class _ContactScreenState extends State<ContactScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   Align(
+                      alignment: Alignment.centerLeft,
+                      child: InkWell(
+                        onTap: () {
+                           Navigator.of(context).pop();
+                         },
+                         child: const Icon(Icons.arrow_back_rounded),
+                 ),
+                ),
+                   const Text(
+                   'Contact Us',
+                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                   const SizedBox(
+                     height: 5,
+                   ),
+
+                   Image.asset('images/contact.png',width: 300,height: 300,),
+                   const SizedBox(height: 13,),
+                   const Card(
+                       child:ListTile(
+                       tileColor: Colors.white24,
+                       leading: Icon(Icons.email, size: 28,color: Colors.deepPurple,),
+                       title: Text('paddy123@gmail.com', style: TextStyle(fontSize: 20, color: Colors.black),),
+                  )),
+                   SizedBox(height: 4,),
+                   const Card(
+                       child: ListTile(
+                       tileColor: Colors.white24,
+                       leading: Icon(Icons.phone, size: 28,color: Colors.deepPurple,),
+                       title: Text('0776677656', style: TextStyle(fontSize: 20, color: Colors.black),),
+                  )),
+                   SizedBox(height: 4,),
+                   const Card(
+                       child: ListTile(
+                       tileColor: Colors.white24,
+                       leading: Icon(Icons.facebook, size: 28,color: Colors.deepPurple,),
+                       title: Text('PaddyApp', style: TextStyle(fontSize: 20, color: Colors.black),),
+                )),
+
+              ],
+         ),
+        ),
+        ),
+     ),
+     );
+
+
   }
 }
